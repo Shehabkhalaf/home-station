06ca294df3b9a78208e6698da11b555088675c9b
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-08-10 02:04:43 +0300

    Finish Sidebar Responsive

--- admin/public/js/global.js
+++ admin/public/js/global.js
@@ -1,7 +1,18 @@
-const openSidebar = document.getElementById("open");
-const closeSidebar = document.getElementById("close-side");
-const navList = document.getElementById("nav-lists");
+const openSidebar = document.getElementById('open');
+const closeSidebar = document.getElementById('close-side');
+const navList = document.getElementById('nav-lists');
+const overlay = document.getElementById('overlay');
 
-openSidebar.addEventListener("click", () => {
-  navList.classList.remove("-left-[999px]")
-})
+openSidebar.addEventListener('click', () => {
+  navList.classList.remove('-left-[999px]');
+  navList.classList.add('left-0');
+  overlay.classList.remove('hidden');
+  overlay.classList.add('block');
+});
+
+closeSidebar.addEventListener('click', () => {
+  navList.classList.add('-left-[999px]');
+  navList.classList.remove('left-0');
+  overlay.classList.remove('block');
+  overlay.classList.add('hidden');
+});
