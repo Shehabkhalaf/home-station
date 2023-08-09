9571455f2e0b3bf6b0a1d51e10a92745ea193a9d
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-08-10 01:27:55 +0300

    Fix add bug

--- admin/public/js/global.js
+++ admin/public/js/global.js
@@ -1,3 +1,7 @@
 const openSidebar = document.getElementById("open");
 const closeSidebar = document.getElementById("close-side");
+const navList = document.getElementById("nav-lists");
 
+openSidebar.addEventListener("click", () => {
+  
+})
