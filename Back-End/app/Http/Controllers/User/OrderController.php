ac99bbe08d6a655b689f145b4f5a760a3e1405aa
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-08-13 02:14:23 +0300

    Orders for user and admin

--- Back-End/app/Http/Controllers/User/OrderController.php
+++ Back-End/app/Http/Controllers/User/OrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserOrders;
+use App\Models\AdminOrder;
+use App\Models\Order;
 use App\Models\User;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
@@ -16,5 +18,30 @@ class OrderController extends Controller
         $user = User::find(auth()->user()->id);
         $orders = UserOrders::collection($user->orders);
         return $this->JsonResponse(200, 'User orders', $orders);
+    }
+    public function makeOrder(Request $request)
+    {
+        $user = User::find(auth()->user()->id);
+        $user_id = $user->id;
+        $user_data = 'name:' . $user->name . '<br>' . 'email:' . $user->email . '<br>' . 'phone:' . $user->phone . '<br>' . 'location:' . $user->address;
+        $order = new Order;
+        $order->user_id = $user_id;
+        $order->order_details = $request->order_details;
+        $order->total_price = $request->total_price;
+        $order->paid = $request->paid_method;
+        $order->promocode = $request->has('promocode') ? $request->promocode : 'nothing';
+        $UserOrdered = $order->save();
+        $adminOrder = new AdminOrder;
+        $adminOrder->user_data = $user_data;
+        $adminOrder->order_details = $request->order_details;
+        $adminOrder->total_price = $request->total_price;
+        $adminOrder->promocode = $order->promocode;
+        $adminOrder->paid = $request->paid_method;
+        $adminOrders = $adminOrder->save();
+        if ($UserOrdered && $adminOrders) {
+            return $this->JsonResponse(200, 'Order Done', $UserOrdered);
+        } else {
+            return $this->JsonResponse(500, 'Somethin went wrong');
+        }
     }
 }
