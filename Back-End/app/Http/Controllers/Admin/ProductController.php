e7b7f991c6188050c4e65975fcdf5f4f62363448
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-08-14 15:40:27 +0300

    push

--- Back-End/app/Http/Controllers/Admin/ProductController.php
+++ Back-End/app/Http/Controllers/Admin/ProductController.php
@@ -73,10 +73,10 @@ class ProductController extends Controller
     public function updateProduct(Request $addProductRequest)
     {
         $product = Product::find($addProductRequest->id);
-        /*$images = explode('|', $product->image);
+        $images = explode('|', $product->image);
         foreach ($images as $image) {
             Storage::delete($image);
-        }*/
+        }
         $product->title = $addProductRequest->input('title');
         $product->description = $addProductRequest->input('description');
         $product->color = $addProductRequest->input('color');
